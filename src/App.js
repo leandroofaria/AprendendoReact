f5ff479515ab8f8244d342ef43f8d2860f880b1c
leandroofaria <leandrogabrielfaria05@gmail.com>
2024-10-28 13:58:59 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  
   const [input, setInput] = useState('');
   const [tarefas, setTarefas] = useState([
     'Pagar a conta de luz', 'Estudar react'
   ]);
 
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem('@tarefa');
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+  }, [tarefas]);
+
   function handleRegistrer(event) {
     event.preventDefault();
+    if (input.trim() === '') return; // Não salva tarefa vazia
     setTarefas([...tarefas, input]);
     setInput('');
-  } 
+  }
 
   return (
     <div>
       <h1>Lista de Tarefas</h1>
       <form onSubmit={handleRegistrer}>
         <label>Nome Da Tarefa:</label><br />
-        <input 
-          placeholder="Digite uma tarefa" 
-          value={input} 
-          onChange={(event) => setInput(event.target.value)} 
+        <input
+          placeholder="Digite uma tarefa"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
         /><br /><br />
 
         <button type="submit">Registrar</button>
@@ -30,8 +41,8 @@ function App() {
       <br />
 
       <ul>
-        {tarefas.map(tarefa => (
-          <li key={tarefa}>{tarefa}</li>
+        {tarefas.map((tarefa, index) => (
+          <li key={index}>{tarefa}</li>
         ))}
       </ul>
     </div>
